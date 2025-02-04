51e62da34f992eaf7ecec9b96fbb00027c414435
sibmaks <sibmaks@vk.com>
2025-02-04 19:11:16 +0300

    [Web App Kafka] Add support JSON Template #9

--- web-app-frontend/src/api/message.templates.ts
+++ web-app-frontend/src/api/message.templates.ts
@@ -7,7 +7,7 @@ const messageTemplate = axios.create({
   },
 });
 
-export type Engine = 'FREEMARKER' | 'AVRO' | 'JAVA_SCRIPT' | 'PYTHON';
+export type Engine = 'FREEMARKER' | 'AVRO' | 'JAVA_SCRIPT' | 'PYTHON' | 'JSON';
 
 export interface MessageTemplateShortRs {
   id: number;
